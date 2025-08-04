e6912b82115b06f9297bae874c6efb9030c3a4f1
whogod <1249773873@qq.com>
2025-08-05 00:40:36 +0800

    启动类可以启动

---
+++ producer/src/main/java/hyt/feign/ProducerApplication.java
@@ -0,0 +1,18 @@
+package hyt.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
